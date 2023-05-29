b694753aeaef1c33a3c8cc4cd6a8012805a499f1
sukesh-pfx <64000239+sukesh-pfx@users.noreply.github.com>
2023-05-29 05:52:41 +0530

    code clean up

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-// // const pwdgen = require("pwdgen");
-// import pwdgen from "pwdgen";
-
-// const test = pwdgen(8, { includeLowerCase: true, includeNumbers: true });
-// console.log(test);
-// import pass from "pack";
-
-// const pass = require("./package/bundle.js");
-// import pass from "package";
-// import pass from "./package/bundle.mjs";
-// import randomPassword from "./package/bundle.js";
-// const pass = require("randompass");
-import pass from "randompass";
+// const pass =require('random-pwd');
+import pass from 'random-pwd';
 const test = pass(8, {
-  includeLowerCase: true,
-  includeNumbers: true,
+	includeSpecialChar: true,
+	includeUpperCase: true,
+	includeLowerCase: true,
+	includeNumbers: true,
 });
 console.log(test);

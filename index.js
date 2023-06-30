cd0d1bab3e1975babb0e5e0c84d945b736355a84
sukesh-s <48245207+sukesh-s@users.noreply.github.com>
2023-06-30 21:26:07 +0530

    demo page updates

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
-import pass from 'random-pwd';
-const test = pass(8, {
-	includeSpecialChar: true,
-	includeUpperCase: true,
-	includeLowerCase: true,
-	includeNumbers: true,
+import randomPassword from 'https://unpkg.com/random-pwd@1.0.2/bundle.mjs';
+
+let options = {
+	numbers: false,
+	lowerChar: true,
+	upperChar: true,
+	specialChar: true,
+	rangeCount: 8,
+};
+
+let countRange = document.querySelector('[type="range"]');
+const checkBoxes = document.querySelectorAll('[type="checkbox"]');
+const countLabel = document.getElementById('range-count');
+const countInfo = document.getElementById('count-info');
+const _password = document.getElementById('random-password');
+const copyIcon = document.getElementById('copy-password');
+
+generatePassword();
+
+for (let i = 0; i < checkBoxes.length; i++) {
+	let checkbox = checkBoxes[i];
+	checkbox.addEventListener('click', function () {
+		options[this.id] = this.checked ? true : false;
+		generatePassword();
+	});
+}
+
+countRange.addEventListener('change', function () {
+	options[this.id] = this.value;
+	generatePassword();
 });
-console.log(test);
+copyIcon.addEventListener('click', function () {
+	const copiedPassword = _password.innerHTML;
+	navigator.clipboard.writeText(copiedPassword);
+});
+
+function generatePassword() {
+	const password = randomPassword(options?.rangeCount, {
+		includeNumbers: options?.numbers,
+		includeLowerCase: options?.lowerChar,
+		includeUpperCase: options?.upperChar,
+		includeSpecialChar: options?.specialChar,
+	});
+	for (let i = 0; i < checkBoxes.length; i++) {
+		let checkbox = checkBoxes[i];
+		checkbox.checked = options[checkbox.id];
+	}
+
+	_password.innerHTML = password ? password : '🧞....';
+	countLabel.innerHTML = options?.rangeCount;
+	countInfo.innerHTML = Number(options?.rangeCount) > 1 ? 'Characters' : 'Character';
+	countRange.value = Number(options?.rangeCount);
+}
